938af5521abc8d62eb843c5422c6ddb7d2a88208
Youssef Yammine <lebenbou@gmail.com>
2023-04-07 21:16:51 +0300

    implemented pushBack and pushFront

--- LinkedList/DoublyLinkedList.cpp
+++ LinkedList/DoublyLinkedList.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <vector>
+#include <cassert>
 #include "./AbstractLinkedList.cpp"
 using namespace std;
 
@@ -14,8 +15,14 @@ private:
         Node* next;
         Node* previous;
 
-        Node(const Type& v, Node* n = nullptr, Node* p = nullptr)
+        Node(const Type& v, Node* p = nullptr, Node* n = nullptr)
         : value(v), next(n), previous(p) {}
+    };
+
+    Node* node_at_index(size_t index) const {
+
+        assert(index < size && "Index out of bounds");
+        return nullptr;
     }
 
     size_t size;
@@ -24,4 +31,46 @@ private:
 
 public:
     DoublyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+    virtual ostream& to_ostream(ostream& output) const = 0;
+
+    void push_back(const Type& new_value){
+
+        if(is_empty()) return push_front(new_value);
+        
+        tail->next = new Node(new_value, tail, nullptr);
+        ++size;
+    }
+
+    void push_front(const Type&){
+        
+        head = new Node(new_value, nullptr, head);
+        if(++size == 1) tail = head;
+    }
+
+    bool pop_back() = 0;
+    bool pop_front() = 0;
+
+    bool insert(const Type& new_value, size_t index) = 0;
+    bool remove(size_t index) = 0;
+
+    Type& operator[](const size_t& index) = 0;
+    Type operator[](const size_t& index) const = 0;
+
+    bool is_empty() const { return !head; }
+    size_t length() const { return size; }
+
+    void clear(){
+        while(pop_front());
+    }
+
+    ~DoublyLinkedList(){
+        this->clear();
+    }
 };
+
+int main(int argc, char const *argv[])
+{
+    DoublyLinkedList<int> dl;
+    return 0;
+}
