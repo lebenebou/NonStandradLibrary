12856e3b3bed9c0b6758d696ceb93eb887cded00
Youssef Yammine <lebenbou@gmail.com>
2023-04-21 20:47:57 +0300

    implemented Insert and Find

--- BST.cpp
+++ BST.cpp
@@ -1,4 +1,5 @@
 
+
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -16,12 +17,57 @@ private:
         Node* left;
 
         Node(const Type& v, Node* r =nullptr, Node* l =nullptr)
-        : value(v), right(r), left(l);
-    }
+        : value(v), right(r), left(l) {}
+    };
 
     size_t count;
     Node* root;
 
+    bool insert(const Type& new_value, Node* start){
+
+        if(is_empty()) return (++count && (root = new Node(new_value)));
+        
+        if(new_value == start->value) return false; // duplicate value
+
+        if(new_value > start->value){
+
+            if(!start->right) return (++count && (start->right = new Node(new_value)));
+            return insert(new_value, start->right);
+        }
+        else{
+
+            if(!start->left) return (++count && (start->left = new Node(new_value)));
+            return insert(new_value, start->left);
+        }
+        return false;
+    }
+
+    bool find(const Type& elt, Node* start) const {
+
+        if(!start) return false;
+        if(elt == start->value) return true;
+
+        if(elt > start->value) return find(elt, start->right);
+        return find(elt, start->left);
+    }
+
 public:
-    BST() : root(nullptr), size(0) {}
+    BST() : root(nullptr), count(0) {}
+
+    bool insert(const Type& new_value){ return insert(new_value, root); }
+
+    bool find(const Type& elt){ return find(elt, root); }
+
+    bool is_empty() const { return !root; }
 };
+
+int main(int argc, char const *argv[])
+{
+    BST<int> myTree;
+
+    myTree.insert(1);
+    cout << myTree.find(0) << endl;
+    cout << myTree.find(1) << endl;
+    
+    return 0;
+}
